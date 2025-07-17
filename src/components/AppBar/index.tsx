af83f096e24628373b488518152beae2799b6a0d
AttAditya <attachment.aditya@gmail.com>
2025-07-17 13:48:17 +0530

    Version Formatting for convinience

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -11,8 +11,22 @@ export function AppBar() {
   }>({});
 
   const showVersions = useCallback(() => {
+    const convertVersion = (v: string): string => {
+      const timestamp: string = v.split("v")[1];
+      const date: Date = new Date(Number(timestamp));
+
+      const year: string = (date.getFullYear()).toString().padStart(4, "0");
+      const month: string = (date.getMonth() + 1).toString().padStart(2, "0");
+      const day: string = (date.getDate()).toString().padStart(2, "0");
+      const hours: string = (date.getHours()).toString().padStart(2, "0");
+      const minutes: string = (date.getMinutes()).toString().padStart(2, "0");
+      const seconds: string = (date.getSeconds()).toString().padStart(2, "0");
+
+      return `v${year}.${month}.${day}.${hours}${minutes}${seconds}`;
+    };
+
     const versionMessage = Object.entries(version).map(
-      v => `${v[0]}: ${v[1]}`
+      v => `${v[0]}: ${convertVersion(v[1])}`
     ).join("\n");
     
     alert(versionMessage);
